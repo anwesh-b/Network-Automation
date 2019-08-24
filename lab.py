bc3f733072f70ad82250be955f7616d3f85e0dc1
Anwesh Budhathoki <abperni@gmail.com>
2019-08-25 02:04:17 +0545

    Eighth Commit

---
+++ lab.py
@@ -0,0 +1,15 @@
+from netmiko import ConnectHandler
+
+iosv_l2 = {
+    'device_type': 'cisco_ios',
+    'ip': '192.168.122.127',
+    'username': 'asian',
+    'password': 'hack',
+    'secret': 'cisco'
+    }
+net_connect = ConnectHandler(**iosv_l2)
+output = net_connect.send_command('show run')
+res = output.index('interface')
+end = output.index('ip forward-protocol nd')
+ok =  output[res:end-3]
+print(ok.split('!'))
