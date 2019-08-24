6e4c03a756283ef854bb189d3eb4324783282b89
Anwesh Budhathoki <abperni@gmail.com>
2019-08-24 20:22:47 +0545

    Fourth Commit

--- dhcp.py
+++ dhcp.py
@@ -1,16 +1,17 @@
 from netmiko import ConnectHandler
+import sys
 
 iosv_l2 = {
     'device_type': 'cisco_ios',
-    'ip': '192.168.122.182',
-    'username': 'anwesh',
-    'password': 'cisco',
+    'ip': '192.168.122.127',
+    'username': 'asian',
+    'password': 'hack',
     'secret': 'cisco'
     }
 net_connect = ConnectHandler(**iosv_l2)
 ok = net_connect.enable()
 cmds = ['conf t',
-        'int fa0/0',
-        'no sh',
-        'ip address dhcp']
+        'int '+ sys.argv[1],
+        'ip address 192.168.1.1 255.255.255.0',
+        'no sh']
 output = net_connect.send_config_set(cmds)
--- index.js
+++ index.js
@@ -10,6 +10,7 @@ var options = {
     pythonPath: '/usr/bin/python', 
     pythonOptions: ['-u'],
     scriptPath: '/home/anwesh/asian hack',
+    args: []
 };
 
 app.get('/',async (req,res)=>{
@@ -17,21 +18,22 @@ app.get('/',async (req,res)=>{
         if (err) throw err;
         console.log(String(results));
         console.log('finished');
-        res.redirect('/');
+        res.render('index.ejs',{results:String(results)});
       });
 })
 
-app.get('/dhcp',async (req,res)=>{
+app.post('/dhcp',async (req,res)=>{
+    options.args.push(req.body.int)
+    // options.push({args: [String(req.body.inter)]})
     PythonShell.run('dhcp.py', options , async (err, results)=> {
         if (err) throw err;
         console.log(String(results));
         console.log('finished');
-        res.render('index.ejs',{results:String(results)});
+        res.redirect('/');
       });
-}
+    }
 )
 
 app.listen(port,()=>{
     console.log("Server is created...\nGoto 127.0.0.1:3000");
 });
-
