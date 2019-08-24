21ae0960621ce46e3a26d69515d7841555dcfbca
Anwesh Budhathoki <abperni@gmail.com>
2019-08-24 17:30:23 +0545

    Second Commit

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const express = require("express");
-// const methodOverride = require("method-override");
 const app = express();
 const port = process.env.PORT||3000;
 app.set('view engine','ejs');
 app.use(express.static(__dirname+"/public"));
 const { PythonShell } = require('python-shell');
-// import { ConnectHandler } from 'netmiko'
+
 var options = {
     mode: 'text',
     pythonPath: '/usr/bin/python', 
     pythonOptions: ['-u'],
-    // make sure you use an absolute path for scriptPath
     scriptPath: '/home/anwesh/asian hack',
-    // args: ['value1', 'value2', 'value3']
   };
 
 app.get('/',async (req,res)=>{
@@ -20,14 +17,9 @@ app.get('/',async (req,res)=>{
         if (err) throw err;
         console.log(String(results));
         console.log('finished');
-        // res.render('index.ejs',{results: results});
         res.render('index.ejs',{results:String(results)});
-        // res.send(5);
       });
 })
-app.use(express.json());
-// app.use(express.urlencoded({extended:false}));
-// app.use(methodOverride("_method"));
 
 app.listen(port,()=>{
     console.log("Server is created...\nGoto 127.0.0.1:3000");
